29f409c4b0eb69b58cb343d55c34a2967c779de6
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2022-01-12 16:14:07 +0100

    Service API

---
+++ app/Http/Controllers/Api/ServiceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ServiceResource;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    public function show(Service $service)
+    {
+        return new ServiceResource($service);
+    }
+}
