0b25d3315893859b2c45e6211093dc764c1cd1fb
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2022-01-27 10:35:39 +0100

    Route fix

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('services', [ServiceController::class, 'index']);
-Route::get('services/{service:key}', [ServiceController::class, 'show']);
+Route::get('services', [ApiController::class, 'index']);
+Route::get('services/{service:key}', [ApiController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
